170318c93803787afa5021e3bc1c140726562f45
ritwik1994 <ritwikenator@gmail.com>
2018-03-14 07:49:52 +0530

    work from ubuntu

--- website/music/models.py
+++ website/music/models.py
@@ -13,3 +13,7 @@ class song(models.Model):
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
     file_type = models.CharField(max_length=10)
     song_name = models.CharField(max_length=250)
+    is_favorite = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.song_name
--- website/music/views.py
+++ website/music/views.py
@@ -1,6 +1,6 @@
-from django.http import Http404
-from django.shortcuts import render
-from .models import Album
+
+from django.shortcuts import render, get_object_or_404
+from .models import Album, song
 
 def index(request):
     # connecting database
@@ -12,12 +12,29 @@ def index(request):
     return render(request, 'music/index.html', context)
 
 def detail(request, album_id):
-    try:
-        album = Album.objects.get(pk=album_id)
-    except Album.DoesNotExist:
-        raise Http404('Album does not exist')
+    # try:
+    #     album = Album.objects.get(pk=album_id)
+    # except Album.DoesNotExist:
+    #     raise Http404('Album does not exist')
+    album = get_object_or_404(Album, pk=album_id)
     context = {
         'album': album
     }
     return render(request, 'music/detail.html', context)
+
+def favorite(request, album_id):
+    album = get_object_or_404(Album, pk=album_id)
+    try:
+        selected_song = album.song_set.get(pk=request.POST['song'])
+    except (KeyError, song.DoesNotExist):
+        context = {
+            'album': album,
+            'error_message': "You didnot select a valid song"
+        }
+        return render(request, 'music/detail.html', context)
+    else:
+        selected_song.is_favorite = True
+        selected_song.save()
+        return render(request, 'music/detail.html', {'album': album})
+
 
